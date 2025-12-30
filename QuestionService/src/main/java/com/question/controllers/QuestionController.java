6a52457f41f30443ce4fde8dd7620c1f856bb19d
Om Pimple <ompimple04@gmail.com>
2025-12-30 21:47:17 +0530

    Initial commit - Spring Boot Microservices Quiz project

---
+++ QuestionService/src/main/java/com/question/controllers/QuestionController.java
@@ -0,0 +1,46 @@
+package com.question.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.question.entities.Question;
+import com.question.services.QuestionService;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+    
+    @Autowired
+    private QuestionService questionService;
+
+    // Create
+    @PostMapping
+    public Question create(@RequestBody Question question) {
+        return questionService.create(question);
+    }
+
+    // Get All Questions
+    @GetMapping
+    public List<Question> getAll() {
+        return questionService.get();
+    }
+
+    // Get One Question
+    @GetMapping("/{questionId}")
+    public Question getQuestion(@PathVariable Long questionId) {
+        return questionService.getOne(questionId);
+    }
+
+    // Get all Questions of a specific Quiz
+    @GetMapping("/quiz/{quizId}")
+    public List<Question> getQuestionsOfQuiz(@PathVariable Long quizId) {
+        return questionService.getQuestionsOfQuiz(quizId);
+    }
+}
